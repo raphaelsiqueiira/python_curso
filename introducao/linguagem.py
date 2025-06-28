78a98198519deffe9d0ed0a2f4f8f61b0639dba3
Raphael Borba <raphaelborba10@gmail.com>
2025-06-28 13:48:36 -0300

    Comite inicial

---
+++ introducao/linguagem.py
@@ -0,0 +1,34 @@
+"""
+Apresentação da linguagem
+
+Simplicidade e facilidade na leitura:
+  - Sintaxe simples e limpa que favorece a legibilidade.
+  - Sua sintaxe busca ser próxima da linguagem humana. Ao invés de chaves {} ou ;, usa indentação (espaços) para definir blocos de código.
+"""
+
+# Exemplo
+idade = 18
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
+"""
+Expressividade e elegância:
+  - Python encorajao uso de abordagens mais diretas e menos verbosas.
+
+Interpretada e de alto nível:
+  - É uma linguagem interpretada, o que significa que o código é executado linha  por linha sem necessidade de compilação explícita.
+
+Tipagem dinâmica:
+  - As variáveis não precisam ser declaradas com um tipo específico.
+   Python descobre isso automaticamente.
+
+Versátil:
+  - Python pode ser usada em várias áreas:
+  Análise de Dados – com pandas, numpy, matplotlib
+  Automações – scripts que automatizam tarefas no computador
+  Desenvolvimento Web – com Flask, Django
+  Cibersegurança – scripts de ataque, auditoria, análise de logs, etc.
+
+"""
